ea7b08d11dcbd6eaa1fe1776777bb9352868e2c4
Nathalia <nathalia.gramst@gmail.com>
2022-09-14 17:16:50 -0300

    Cleaning react-app, creating routes for login and sign-up, creating reset.css

---
+++ src/services/routta.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000";
+
+function createHeaders() {
+    const auth = JSON.parse(localStorage.getItem("mywallet"));
+    const config = {
+        headers: {
+            Authorization: `Bearer ${auth.token}`
+        }
+    };
+
+    return config;
+}
+
+function postSignUp(body) {
+    const promise = axios.post(`${BASE_URL}/auth/sign-up`, body);
+    return promise;
+}
+
+function postSignIn(body) {
+    const promise = axios.post(`${BASE_URL}/auth/sign-in`, body);
+    return promise;
+}
+
+export {
+    postSignUp,
+    postSignIn
+}
